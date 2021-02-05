191391bbd8f0bbf534cfffd1169f4a50e8cb51a8
Moaaz-Rafique <62691687+Moaaz-Rafique@users.noreply.github.com>
2021-02-05 19:28:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
-function setPost(){
+function setPost() {
     var inp = document.getElementById("inj")
     var main = document.getElementById("main")
-    var div= document.createElement("div")
-   if(inp.value===""){
-       return
-   }
+    var div = document.createElement("div")
+    if (inp.value === "") {
+        return
+    }
     main.appendChild(div)
-    var h2= document.createElement("h2")
-    var closeButton= document.createElement("button")
-    var editButton= document.createElement("button")
-    closeButton.setAttribute("class","btn")
-    editButton.setAttribute("class","btn")
-    editButton.innerHTML="Edit"
-    
-    closeButton.onclick=function(){
+    var todoText = document.createElement("input")
+    todoText.setAttribute("class","todoTxt")
+    var flag = true;
+    todoText.disabled = flag;
+    var closeButton = document.createElement("button")
+    var editButton = document.createElement("button")
+    closeButton.setAttribute("class", "btn")
+    editButton.setAttribute("class", "btn")
+    editButton.innerHTML = "Edit"
+
+    closeButton.onclick = function () {
         div.remove()
     }
-    
-    closeButton.innerHTML="Delete"
-    div.appendChild(h2)
-    
-    div.setAttribute("class",'card')
-    var h2Text= document.createTextNode(inp.value)
-    editButton.onclick=function(){   
-        var newtxt=prompt("Enter your edit")
-        if(newtxt!=""){
-            
-        h2.removeChild(h2Text)       
-        h2Text= document.createTextNode(newtxt)
-        h2.appendChild(h2Text)
+
+    closeButton.innerHTML = "Delete"
+    div.appendChild(todoText)
+    div.setAttribute("class", 'card')    
+    editButton.onclick = function () {
+        if (flag) {
+            editButton.innerHTML = "Update"
+            flag = false
+            todoText.disabled = flag  
+            todoText.focus()        
         }
-    }    
+        else{
+            editButton.innerHTML = "Edit"
+            flag = true
+            todoText.disabled = flag            
+        }
+
+    }
     div.appendChild(closeButton)
     div.appendChild(editButton)
-    h2.appendChild(h2Text)
-   
+    todoText.value=inp.value
+
 }
-function delAll(){
+function delAll() {
     var main = document.getElementById("main")
-    main.innerHTML=""
+    main.innerHTML = ""
 }
