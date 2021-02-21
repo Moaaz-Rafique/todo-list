c1388612603c746228a608a506259e47cba904a9
Moaaz <Moaaz@HomeMzr>
2021-02-21 22:38:44 +0500

    add firebase connection

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+var db = firebase.database().ref('/posts/');
+getAllPosts()
 function setPost() {
     var inp = document.getElementById("inj")
+    var inpText = inp.value
+    addPostToList(addToDataBase(inpText),inpText)
+    
+}
+function addPostToList(id,inpText) {
     var main = document.getElementById("main")
     var div = document.createElement("div")
-    if (inp.value === "") {
+    div.setAttribute("id",id)
+    if (inpText === "") {
         return
     }
     main.appendChild(div)
     var todoText = document.createElement("input")
-    todoText.setAttribute("class","todoTxt")
+    todoText.setAttribute("class", "todoTxt")
     var flag = true;
     todoText.disabled = flag;
     var closeButton = document.createElement("button")
@@ -18,36 +26,52 @@ function setPost() {
 
     closeButton.onclick = function () {
         div.remove()
+        db.child(id).remove()
     }
 
     closeButton.innerHTML = "Delete"
     div.appendChild(todoText)
-    div.setAttribute("class", 'card')    
-    var prev=""
+    div.setAttribute("class", 'card')
+    var prev = ""
     editButton.onclick = function () {
         if (flag) {
-            prev=todoText.value
+            prev = todoText.value
             editButton.innerHTML = "Update"
             flag = false
-            todoText.disabled = flag  
-            todoText.focus()        
+            todoText.disabled = flag
+            todoText.focus()
         }
-        else{
-            if(todoText.value===""){
-                todoText.value=prev
+        else {
+            if (todoText.value === "") {
+                todoText.value = prev
             }
             editButton.innerHTML = "Edit"
+            db.child(id).set({postData: todoText.value})
             flag = true
-            todoText.disabled = flag              
+            todoText.disabled = flag
         }
 
     }
     div.appendChild(closeButton)
     div.appendChild(editButton)
-    todoText.value=inp.value
+    todoText.value = inpText
 
 }
 function delAll() {
     var main = document.getElementById("main")
     main.innerHTML = ""
+    db.remove()
+}
+function addToDataBase(inpText){
+    return (db.push({
+        postData: inpText
+    }).key)
+
+}
+function getAllPosts(){
+    firebase.database().ref('/posts/').once('value',function(data){
+        for(var d in data.val()){
+            addPostToList(d,data.val()[d].postData)
+        }
+ })
 }
